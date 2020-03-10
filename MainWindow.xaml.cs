81bd5f9d4cf3ad0b813c1cb4fd524bd53a864f6a
sanjayit89 <sanjay.it89@gmail.com>
2020-03-11 00:34:31 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ResponsiveLayout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnADD_Click(object sender, RoutedEventArgs e)
+        {
+            //txtMessageListInput.Text = txtDCCHInput.Text;
+            txtMessageListInput.Items.Add(txtDCCHInput.Text);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            string lstCount = txtMessageListInput.SelectedItem.ToString();
+            MessageBox.Show(lstCount);
+        }
+
+        private void btnAnalyze_Click(object sender, RoutedEventArgs e)
+        {
+            DialogBox dialogBox = new DialogBox();
+            dialogBox.ShowDialog();
+            
+        }
+    }
+ }
+
